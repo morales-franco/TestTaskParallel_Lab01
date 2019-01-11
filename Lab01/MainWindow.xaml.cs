13cb9735f3162b296d23f314b8cd6f20fcab0e45
Franco Morales <franco.e.morales@gmail.com>
2019-01-11 13:08:53 -0300

    Notes

--- Lab01/MainWindow.xaml.cs
+++ Lab01/MainWindow.xaml.cs
@@ -24,15 +24,16 @@ namespace Lab01
         public MainWindow()
         {
             InitializeComponent();
-            //CreateTask();
+            CreateTask();
             //RunTaskGroupAndWaitAll();
             //RunTaskGroupAndWaitAny();
-            ReturnTaskValue();
+            //ReturnTaskValue();
         }
 
         void CreateTask()
         {
             //La clase Task permite realizar multiples tareas al mismo tiempo, cada una en thread distinto
+            //Una Task ejecuta un bloque de codigo
             //Tarea que va a ejecutar un Action
             Task T1;
 
@@ -40,14 +41,20 @@ namespace Lab01
             Action code = new Action(ShowMessage);
 
             //La tarea va a ejecutar el delegado que le pasamos en el constructor.
-            T1 = new Task(ShowMessage);
+            T1 = new Task(code);
 
+            //Si no volvemos a utilizar el Delegado entonces podemos pasarle directamente el Metodo
+            //o generar un Delegado con metodo anonimo
+            //T1 = new Task(ShowMessage);
+
+            //Delegado con metodo anonimo
             Task T2 = new Task(delegate
             {
                 MessageBox.Show("Ejecutando una tarea en un delegado anonimo!");
             });
 
-            //(Paramteros de entrada) => Expression
+            //Crear tarea mediante una Labda expression
+            //(Paramtros de entrada) => Expression
             Task T3 = new Task(() => ShowMessage());
 
             Task T4 = new Task(() => MessageBox.Show("Ejecutando la tarea 4"));
@@ -67,16 +74,19 @@ namespace Lab01
 
             //Cuando stateamos la Task la bibilioteca TaskParallel asigna un Thread a esa Tarea 
             //y esta comieza a ejecutarse en un Thread Independiente
-            //Entonces la task T7 y el Thead Princiapl se ejecutaran en Paralelo
+            //Entonces la task T7 y el Thead Principal se ejecutaran en Paralelo
             T7.Start();
 
             AddMessage("Ejecutando en el Main Thread");
 
             /*
-             * Podemos delegar la creación de Tasks a la TaskFactory para crear y poner en cola una tarea
+             * Podemos delegar la creación de Tasks a la TaskFactory para crear y poner en cola una tarea. 
+             * Nos permite iniciar y Ejecutar en una unica linea de codigo.
              */
             Task T8 = Task.Factory.StartNew(() => AddMessage("Task T8 Iniciada con TaskFactory"));
 
+            //Metodo Run crea y ejecuta al mismo tiempo
+            //Run() es un atajo / shorcut del factory
             //Task.Factory. es altamente configurable, si solo deseamos ejecutar algún código sin opciones adicionales de ejecución --> Podemos utilizar:
             var T9 = Task.Run(() => AddMessage("Task T9 ejecutada desde Task Run"));
 
@@ -103,11 +113,24 @@ namespace Lab01
 
         void AddMessage(string message)
         {
+            //Messages No puede ser accedido por otro thread que no sea el principal
+            //Messages pertenece a la UI, solo puede ser accedido con principal thread
+
             //Es necesario utilizar el Distpacher ya que sino da Exception cuando la T7 quiere modificar la UI
             //Esto sucede en wpf cuando un thead distinto al principal intenta modificar elementos de la interfaz de usuario.
 
             int currentThreadID = Thread.CurrentThread.ManagedThreadId;
 
+            //Con la opción Dispatcher le decis que ejecute el codigo que le pasas desde el hilo principal.
+            //Por esta razon tomamamos el ManagedThreadId fuera del Dispacher porque sino ManagedThreadId siempre seria el principal Thread
+            /*
+             * this.Dispatcher.Invoke(() =>
+            {
+                Messages.Content +=
+                $"Mensaje: {message}" +
+                $"Hilo Actual: { Thread.CurrentThread.ManagedThreadId } \n"; --> Siempre mostraria el ManagedThreadId del hilo principal
+            });
+             */
             this.Dispatcher.Invoke(() =>
             {
                 Messages.Content +=
