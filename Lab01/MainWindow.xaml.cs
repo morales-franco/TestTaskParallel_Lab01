5ba79178b4b89f08e6f67f2b26b1bab5569c4770
Morales Franco <franco.e.morales@gmail.com>
2019-01-13 20:21:03 -0300

    Lab 01. Finalizado

--- Lab01/MainWindow.xaml.cs
+++ Lab01/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace Lab01
         public MainWindow()
         {
             InitializeComponent();
-            CreateTask();
+            //CreateTask();
             //RunTaskGroupAndWaitAll();
             //RunTaskGroupAndWaitAny();
             //ReturnTaskValue();
@@ -99,6 +99,9 @@ namespace Lab01
             });
 
             WriteToOutput($"Esperando a la tarea 10 - Segundos Now: { DateTime.Now.Second }");
+
+            //Esperamos a que se termine de ejecutar la T10 esto bloquea el Hilo Principal
+            //Es decir NO veriamos la UI hasta que la T10 termine
             T10.Wait();
             WriteToOutput($"Finalizo tarea 10 - Segundos Now: { DateTime.Now.Second }");
 
@@ -121,8 +124,8 @@ namespace Lab01
 
             int currentThreadID = Thread.CurrentThread.ManagedThreadId;
 
-            //Con la opción Dispatcher le decis que ejecute el codigo que le pasas desde el hilo principal.
-            //Por esta razon tomamamos el ManagedThreadId fuera del Dispacher porque sino ManagedThreadId siempre seria el principal Thread
+            //Con la opción Dispatcher le decis que ejecute el codigo que le pasas desde el hilo principal. (El codigo que le pasemos lo ejecuta el Main Thread)
+            //Por esta razon tomamos el ManagedThreadId fuera del Dispacher porque sino ManagedThreadId siempre seria el principal Thread
             /*
              * this.Dispatcher.Invoke(() =>
             {
@@ -134,7 +137,7 @@ namespace Lab01
             this.Dispatcher.Invoke(() =>
             {
                 Messages.Content +=
-                $"Mensaje: {message}" +
+                $"Mensaje: {message} " +
                 $"Hilo Actual: { currentThreadID } \n";
             });
 
@@ -168,7 +171,7 @@ namespace Lab01
             };
 
             WriteToOutput($"Esperando a que Finalice TODO el conjunto de Tareas - Segundos Now: { DateTime.Now.Second }");
-            //Task.WaitAll Espera que finalicen todas las tareas lanzadas en el array de task
+            //Task.WaitAll: El hilo principal espera a que todas las tareas lanzadas en el array de task finalicen su ejecución
             Task.WaitAll(taskGroup);
             WriteToOutput($"Todas las tareas del Group han Finalizado - Segundos Now: { DateTime.Now.Second }");
         }
@@ -185,7 +188,7 @@ namespace Lab01
             };
 
             WriteToOutput($"Esperando a que Finalice ALGUNA TAREA del conjunto - Segundos Now: { DateTime.Now.Second }");
-            //Task.WaitAll Espera que finalicen todas las tareas lanzadas en el array de task
+            //Task.WaitAny Espera que al menos una tarea finalice (pueden ser varias pero al menos 1 termino)
             Task.WaitAny(taskGroup);
             WriteToOutput($"ALGUNA las tareas del Group han Finalizado - Segundos Now: { DateTime.Now.Second }");
         }
@@ -197,7 +200,7 @@ namespace Lab01
             T = Task.Run<int>(() => new Random().Next(1000));
 
             //Cuando el Main Thread intenta acceder a la propiedad Result y la task aun NO ha terminado
-            //Entonces el Main Thread espera hasta que el Resultado este disponible.
+            //Entonces el Main Thread espera hasta que el Resultado este disponible. ==> Hce que la operación se vuelva Sincrona
             WriteToOutput($"Valor devuelto por la tarea: { T.Result }");
 
             Task<int> T2 = Task.Run<int>(() =>
@@ -211,5 +214,188 @@ namespace Lab01
             WriteToOutput($"Valor devuelto por la TAREA 2: { T2.Result }");
             WriteToOutput("Fin de ejecución del método ReturnTaskValue...");
         }
+
+        #region Cancelar Tarea
+
+        /*
+         * En algunos casos necesitamos cancelar tareas de larga duración.
+         * Para esto utilizamos los Cancellation Tokens de la biblioteca Task parallel.
+         * 
+         */
+
+        CancellationTokenSource CTS;
+        CancellationToken CT;
+        Task LongRunningTask;
+
+        private void StartTask_Click(object sender, RoutedEventArgs e)
+        {
+            CTS = new CancellationTokenSource();
+            //Obtenemos el token
+            CT = CTS.Token;
+
+            /*
+             * Cuando cancelamos una tarea si no nos interesa saber el Porque se cancelo entonces podemos solamente pasar
+             * el Token de cancelación como Parametro al delegado, y no como argumento al Run.
+             *  LongRunningTask = Task.Run(() =>
+                {
+                    DoLongRunningTask(CT);
+                });
+             * 
+             * De esta forma siempre el estado de la Task al momento de cancelar o finalizar será: RanToCompletion.
+             * Ahora si necesitamos saber si la tarea finalizo a causa de un pedido del usuario (mediante el CT) o porque finalizo la task
+             * necesitamos pasarle el CT como parametro al Run.
+             */
+            //LongRunningTask = Task.Run(() =>
+            //{
+            //    DoLongRunningTask(CT);
+            //}, CT);
+
+
+            /*
+             * Para manejar la TaskCanceledException en el hilo que lanzo la task debemos esperar que finalice la ejecución mediante wait()
+             */
+
+            //######## Manejando TaskCanceledException  ########
+
+            //Alternativa 1 ==> MAL!
+            //NO manejamos la excepción debido a que la exception se da en otro hilo.
+            //Por lo tanto NUNCA salta el catch, el hilo queda en estado Cancelado por el ThrowIfCancellationRequested
+            //pero NO se maneja la exception.
+            //try
+            //{
+            //    LongRunningTask = Task.Run(() =>
+            //    {
+            //        DoLongRunningTask(CT);
+            //    }, CT);
+
+            //}
+            //catch (Exception ex)
+            //{
+
+            //}
+
+            //Alternativa2 ==> MAL!
+            //En este caso manejamos la exception en el misma Task, si NOSOTROS manejamos la exception
+            //Entonces NO lo hace la bibilioteca Task Parallel Library ==> La Task NO queda como Cancelada!
+            //Porque TPL maneja la exception en el hilo que invoco la task!
+            //Si la exception NO llega en este caso al main thread no se cancela.
+
+            //LongRunningTask = Task.Run(() =>
+            //{
+            //    try
+            //    {
+            //        DoLongRunningTask(CT);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //    }
+
+            //}, CT);
+
+            //Alternativa 3 ==> NO !
+            /*
+             * Conceptualmente esta bien: el HILO principal tiene que esperar a terminar el LongRunningTask con wait
+             * y luego manejamos la exception. Pero aca tenemos un deadlock.
+             * El Hilo principal manda a ejecutar el LongRunningTask ==> Lo encola
+             * El hilo principal continua y se queda a la espera que LongRunningTask finalice la ejecución. Hilo principal BLOQUEADO.
+             * El Hilo 2 intenta utilizar el metodo AddMessage() ==> Que en un punto tiene un Dispatcher.Invoke() para decirle al hilo principal que ejecuta una operación.
+             * Pero el thread principal esta BLOQUEADO ! ==> Entonces el hilo 2 le dice al hilo principal hace esto! pero el hilo principal le dice estoy bloqueado esperandote
+             * y no puedo hacer nada. Entonces Hilo2 espera al main thread ==> main thread espera al hilo2 ==> ambos se esperan eternamente ==> DEADLOCK!
+             */
+            //LongRunningTask = Task.Run(() =>
+            //{
+            //    DoLongRunningTask(CT);
+            //}, CT);
+
+            //try
+            //{
+            //    LongRunningTask.Wait();
+            //}
+            //catch (Exception)
+            //{
+            //}
+
+            //Alternativa 4 ==> OK
+            // Para manejar la TaskCanceledException en el hilo que lanzo la task debemos esperar que finalice 
+            // la ejecución mediante wait()
+            Task.Run(() =>
+            {
+                LongRunningTask = Task.Run(() =>
+                {
+                    DoLongRunningTask(CT);
+                }, CT);
+
+            //Controlamos la exception en el thread que invoco la task LongRunningTask
+                try
+                {
+                    LongRunningTask.Wait();
+                }
+                catch (AggregateException ex)
+                {
+                    foreach (var inner in ex.InnerExceptions)
+                    {
+                        if (inner is TaskCanceledException)
+                        {
+                            AddMessage("Tarea Cancelada y TaskCanceledException manejada.");
+                        }
+                        else
+                        {
+                            AddMessage(inner.Message);
+                        }
+                    }
+                }
+            });
+        }
+
+        private void DoLongRunningTask(CancellationToken ct)
+        {
+            List<int> ids = new List<int>() { 1, 3, 4, 7, 11, 18, 29, 47, 76, 100 };
+
+            /*
+             * Si se solicita una cancelación entonces se activa ThrowIfCancellationRequested() del cancellation token.
+             * Lanzando un OperationCanceledException.
+             * La biblioteca Task Parallel cuando se produce una OperationCanceledException:
+             * 1) Examina el CT para ver si efectivamente se pidio una cancelación. Si es así:
+             * 1.1) Maneja la OperationCanceledException
+             * 1.2) Establece el status de la task en Canceled
+             * 1.3) Lanza una: TaskCanceledException empaquetada en una AggregateException
+             * 1.4) Si NO manejamos la exception y luego consultamos el status mostrara ==> Canceled
+             */
+
+            //try
+            //{
+            foreach (var value in ids)
+            {
+
+                ct.ThrowIfCancellationRequested();
+                AddMessage($"Procesando ID: {value} - DoLongRunningTask");
+                Thread.Sleep(2000);
+                //AddMessage("Proceso cancelado correctamente");
+            }
+
+            //}
+            //catch (TaskCanceledException ex)
+            //{
+
+            //    if (ct.IsCancellationRequested)
+            //    {
+            //        //Lógica para finalizar la tarea
+            //        AddMessage("Proceso cancelado");
+            //    }
+            //}
+        }
+
+        private void CancelTask_Click(object sender, RoutedEventArgs e)
+        {
+            //Solicitamos cancelación
+            CTS.Cancel();
+        }
+
+        private void ShowStatus_Click(object sender, RoutedEventArgs e)
+        {
+            AddMessage($"Status de la tarea: { LongRunningTask.Status }");
+        }
+
+        #endregion
     }
 }
